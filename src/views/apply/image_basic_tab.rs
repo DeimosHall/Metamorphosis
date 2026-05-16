f365e65be5c8f7cabe486b73b9ed2c6076bd6269
deimos <deimoshall.dev@proton.me>
2026-05-16 14:31:09 -0600

    refactor: image basic tab to separate files

---
+++ src/views/apply/image_basic_tab.rs
@@ -0,0 +1,163 @@
+use std::path::Path;
+
+use derivative::Derivative;
+use exiftool::ExifToolError;
+use glib::subclass::types::ObjectSubclass;
+use gtk::TemplateChild;
+use gtk::{glib, prelude::*, subclass::prelude::*};
+
+use crate::services::exif::ExifService;
+
+mod imp {
+    use glib::subclass::object::ObjectImpl;
+    use gtk::subclass::widget::CompositeTemplateClass;
+    use gtk::subclass::{box_::BoxImpl, widget::WidgetImpl};
+    use gtk::CompositeTemplate;
+
+    use super::*;
+
+    #[derive(Debug, CompositeTemplate, Derivative)]
+    #[derivative(Default)]
+    #[template(resource = "/dev/deimoshall/Metamorphosis/ui/views/apply/image_basic_tab.ui")]
+    pub struct ImageBasicTab {
+        #[template_child]
+        pub create_date_entry: TemplateChild<gtk::Entry>,
+        #[template_child]
+        pub offset_time_entry: TemplateChild<gtk::Entry>,
+        #[template_child]
+        pub manufacturer_entry: TemplateChild<gtk::Entry>,
+        #[template_child]
+        pub model_entry: TemplateChild<gtk::Entry>,
+        #[template_child]
+        pub image_description_entry: TemplateChild<gtk::Entry>,
+    }
+
+    #[::glib::object_subclass]
+    impl ObjectSubclass for ImageBasicTab {
+        const NAME: &'static str = "ImageBasicTab";
+        type Type = super::ImageBasicTab;
+        type ParentType = gtk::Box;
+
+        fn class_init(klass: &mut Self::Class) {
+            klass.bind_template();
+        }
+
+        fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
+            obj.init_template();
+        }
+    }
+
+    impl ObjectImpl for ImageBasicTab {}
+    impl WidgetImpl for ImageBasicTab {}
+    impl BoxImpl for ImageBasicTab {}
+}
+
+glib::wrapper! {
+    pub struct ImageBasicTab(ObjectSubclass<imp::ImageBasicTab>)
+    @extends gtk::Widget, gtk::Box,
+    @implements gtk::Accessible, gtk::Buildable, gtk::ConstraintTarget;
+}
+
+impl ImageBasicTab {
+    pub fn new() -> Self {
+        glib::Object::new()
+    }
+
+    pub fn date(&self) -> String {
+        self.imp().create_date_entry.text().to_string()
+    }
+
+    pub fn offset(&self) -> String {
+        self.imp().offset_time_entry.text().to_string()
+    }
+
+    pub fn manufacturer(&self) -> String {
+        self.imp().manufacturer_entry.text().to_string()
+    }
+
+    pub fn model(&self) -> String {
+        self.imp().model_entry.text().to_string()
+    }
+
+    pub fn description(&self) -> String {
+        self.imp().image_description_entry.text().to_string()
+    }
+
+    pub fn set_date(&self, date: &str) {
+        self.imp().create_date_entry.set_text(date);
+    }
+
+    pub fn set_offset(&self, offset: &str) {
+        self.imp().offset_time_entry.set_text(offset);
+    }
+
+    pub fn set_manufacturer(&self, manufacturer: &str) {
+        self.imp().manufacturer_entry.set_text(manufacturer);
+    }
+
+    pub fn set_model(&self, model: &str) {
+        self.imp().model_entry.set_text(model);
+    }
+
+    pub fn set_description(&self, description: &str) {
+        self.imp().image_description_entry.set_text(description);
+    }
+
+    // TODO: maybe these methods should go in a trait
+    /// Populate UI fields using exif data from the given file
+    pub fn load_from_file(&self, path: &Path) {
+        let date = ExifService::create_date(path).unwrap_or_default();
+        let offset = ExifService::offset_time(path).unwrap_or_default();
+        let manufacturer = ExifService::make(path).unwrap_or_default();
+        let model = ExifService::model(path).unwrap_or_default();
+        let description = ExifService::image_description(path).unwrap_or_default();
+
+        self.set_date(&date);
+        self.set_offset(&offset);
+        self.set_manufacturer(&manufacturer);
+        self.set_model(&model);
+        self.set_description(&description);
+    }
+
+    /// Take the values from the UI fields and apply them to a file
+    pub fn apply_changes(&self, path: String) -> Result<(), Vec<ExifToolError>> {
+        let path = Path::new(path.as_str());
+        let date = self.date();
+        let offset = self.offset();
+        let manufacturer = self.manufacturer();
+        let model = self.model();
+        let description = self.description();
+
+        let mut errors = Vec::new();
+
+        if let Err(e) = ExifService::set_all_dates(path, date.as_str()) {
+            errors.push(e);
+        }
+
+        if let Err(e) = ExifService::set_all_offset_times(path, offset.as_str()) {
+            errors.push(e);
+        }
+
+        if let Err(e) = ExifService::set_make(path, manufacturer.as_str()) {
+            errors.push(e);
+        }
+
+        if let Err(e) = ExifService::set_model(path, model.as_str()) {
+            errors.push(e);
+        }
+
+        if let Err(e) = ExifService::set_image_description(path, description.as_str()) {
+            errors.push(e);
+        }
+
+        if let Err(e) = ExifService::set_software(path) {
+            errors.push(e);
+        }
+
+        if errors.is_empty() {
+            return Ok(());
+        } else {
+            Err(errors)
+        }
+    }
+}
--- src/views/apply/mod.rs
+++ src/views/apply/mod.rs
@@ -7,17 +7,16 @@ use gtk::{
     prelude::{ButtonExt, EditableExt},
 };
 
-use crate::{
-    components::image_thumbnail::ImageThumbnail, input_file::InputFile,
-    services::exif::ExifService,
-};
+use crate::{components::image_thumbnail::ImageThumbnail, input_file::InputFile};
+
+mod image_basic_tab;
 
 mod imp {
     use adw::subclass::prelude::*;
     use derivative::Derivative;
     use gtk::CompositeTemplate;
 
-    use crate::views::apply;
+    use crate::views::apply::{self, image_basic_tab::ImageBasicTab};
 
     use super::*;
 
@@ -30,15 +29,7 @@ mod imp {
         #[template_child]
         pub image_thumbnail: TemplateChild<ImageThumbnail>,
         #[template_child]
-        pub create_date_entry: TemplateChild<gtk::Entry>,
-        #[template_child]
-        pub offset_time_entry: TemplateChild<gtk::Entry>,
-        #[template_child]
-        pub manufacturer_entry: TemplateChild<gtk::Entry>,
-        #[template_child]
-        pub model_entry: TemplateChild<gtk::Entry>,
-        #[template_child]
-        pub image_description_entry: TemplateChild<gtk::Entry>,
+        pub image_basic_tab: TemplateChild<ImageBasicTab>,
         #[template_child]
         pub apply_button: TemplateChild<gtk::Button>,
     }
@@ -115,114 +106,28 @@ impl Apply {
 
     pub fn set_on_apply<F>(&self, on_apply: F)
     where
+        // TODO: refactor this implementation
         F: Fn(&Apply, String, String, String, String, String) + 'static,
     {
         let view = self.clone();
         self.imp().apply_button.connect_clicked(move |_| {
             let imp = view.imp();
-            let date = imp.create_date_entry.text().to_string();
-            let offset = imp.offset_time_entry.text().to_string();
-            let manufacturer = imp.manufacturer_entry.text().to_string();
-            let model = imp.manufacturer_entry.text().to_string();
-            let description = imp.image_description_entry.text().to_string();
+            let date = imp.image_basic_tab.date();
+            let offset = imp.image_basic_tab.offset();
+            let manufacturer = imp.image_basic_tab.manufacturer();
+            let model = imp.image_basic_tab.model();
+            let description = imp.image_basic_tab.description();
             on_apply(&view, date, offset, manufacturer, model, description);
         });
     }
 
-    pub fn date(&self) -> String {
-        self.imp().create_date_entry.text().to_string()
-    }
-
-    pub fn offset(&self) -> String {
-        self.imp().offset_time_entry.text().to_string()
-    }
-
-    pub fn manufacturer(&self) -> String {
-        self.imp().manufacturer_entry.text().to_string()
-    }
-
-    pub fn model(&self) -> String {
-        self.imp().model_entry.text().to_string()
-    }
-
-    pub fn description(&self) -> String {
-        self.imp().image_description_entry.text().to_string()
-    }
-
-    pub fn set_date(&self, date: &str) {
-        self.imp().create_date_entry.set_text(date);
-    }
-
-    pub fn set_offset(&self, offset: &str) {
-        self.imp().offset_time_entry.set_text(offset);
-    }
-
-    pub fn set_manufacturer(&self, manufacturer: &str) {
-        self.imp().manufacturer_entry.set_text(manufacturer);
-    }
-
-    pub fn set_model(&self, model: &str) {
-        self.imp().model_entry.set_text(model);
-    }
-
-    pub fn set_description(&self, description: &str) {
-        self.imp().image_description_entry.set_text(description);
-    }
-
     /// Populate UI fields using exif data from the given file
     pub fn load_from_file(&self, path: &Path) {
-        let date = ExifService::create_date(path).unwrap_or_default();
-        let offset = ExifService::offset_time(path).unwrap_or_default();
-        let manufacturer = ExifService::make(path).unwrap_or_default();
-        let model = ExifService::model(path).unwrap_or_default();
-        let description = ExifService::image_description(path).unwrap_or_default();
-
-        self.set_date(&date);
-        self.set_offset(&offset);
-        self.set_manufacturer(&manufacturer);
-        self.set_model(&model);
-        self.set_description(&description);
+        self.imp().image_basic_tab.load_from_file(path);
     }
 
     /// Take the values from the UI fields and apply them to a file
     pub fn apply_changes(&self, path: String) -> Result<(), Vec<ExifToolError>> {
-        let path = Path::new(path.as_str());
-        let date = self.date();
-        let offset = self.offset();
-        let manufacturer = self.manufacturer();
-        let model = self.model();
-        let description = self.description();
-
-        let mut errors = Vec::new();
-
-        if let Err(e) = ExifService::set_all_dates(path, date.as_str()) {
-            errors.push(e);
-        }
-
-        if let Err(e) = ExifService::set_all_offset_times(path, offset.as_str()) {
-            errors.push(e);
-        }
-
-        if let Err(e) = ExifService::set_make(path, manufacturer.as_str()) {
-            errors.push(e);
-        }
-
-        if let Err(e) = ExifService::set_model(path, model.as_str()) {
-            errors.push(e);
-        }
-
-        if let Err(e) = ExifService::set_image_description(path, description.as_str()) {
-            errors.push(e);
-        }
-
-        if let Err(e) = ExifService::set_software(path) {
-            errors.push(e);
-        }
-
-        if errors.is_empty() {
-            return Ok(());
-        } else {
-            Err(errors)
-        }
+        self.imp().image_basic_tab.apply_changes(path)
     }
 }
